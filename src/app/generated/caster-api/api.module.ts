2b7b4643ccbcc3534ef6553a94290f7167867e0c
Nic O'Connor <noconnor@cert.org>
2021-02-05 12:20:06 -0500

    Upgrade to angular 10 forced

--- src/app/generated/caster-api/api.module.ts
+++ src/app/generated/caster-api/api.module.ts
@@ -42,7 +42,7 @@ import { WorkspacesService } from './api/workspaces.service';
     WorkspacesService ]
 })
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
         return {
             ngModule: ApiModule,
             providers: [ { provide: Configuration, useFactory: configurationFactory } ]
