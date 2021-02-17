0b170654678a06c5351b1c2ec8aac7028108a271
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-02-17 11:11:19 -0500

    add missing header

--- src/app/shared/utilities/functions.ts
+++ src/app/shared/utilities/functions.ts
@@ -1,3 +1,5 @@
+// Copyright 2021 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 export function isUpdate<T>(obj: any): obj is T {
   return !(
     Object.keys(obj).length === 0 &&
