b8fc85263a374fcab16b93c2b970558038c11afa
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2026-07-09 20:56:42 +0000

    update current user badge

--- src/app/shared/components/current-user-badge/current-user-badge.component.ts
+++ src/app/shared/components/current-user-badge/current-user-badge.component.ts
@@ -3,7 +3,7 @@ Copyright 2021 Carnegie Mellon University. All Rights Reserved.
  Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 */
 
-import { Component, inject, Input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatChipsModule } from '@angular/material/chips';
 import { CurrentUserQuery } from 'src/app/users/state';
@@ -15,17 +15,18 @@ import { CurrentUserQuery } from 'src/app/users/state';
   imports: [MatChipsModule],
 })
 export class CurrentUserBadgeComponent {
-  @Input()
-  userId: string;
+  readonly userId = input<string | null>(null);
 
   private readonly currentUserQuery = inject(CurrentUserQuery);
 
-  private currentUserId = toSignal(
+  private readonly currentUserId = toSignal(
     this.currentUserQuery.select((s) => s.id),
     { initialValue: '' }
   );
 
-  isCurrentUser() {
-    return this.userId === this.currentUserId();
-  }
+  readonly isCurrentUser = computed(() => {
+    const userId = this.userId();
+
+    return !!userId && userId === this.currentUserId();
+  });
 }
